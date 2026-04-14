eb6d9302c4957644c62a1e764fbbf38a636ee2ec
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-04-14 22:15:41 +0530

    typescript

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello ${person}, welocm to the world`;
+}
+const username = "this is new peroject";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person:string):string{
+    return `Hello ${person}, welocm to the world`
+}
+
+const username :string= "this is new peroject";
+console.log(greet(username));
