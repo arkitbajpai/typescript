ab38a87fd1328a9ac9d4fb3118f44e5406f913f6
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-04-15 23:31:12 +0530

    moretypesints

---
+++ src/moretypes.ts
@@ -0,0 +1,43 @@
+let response:any ="42";
+
+let numericLength:number = (response as string).length
+// this is forcefull type assertion becasue we are forcing to tell typscript that it is string
+
+
+type book={
+    name: string
+}
+
+let bookString='{"name": "whow mooved"}';
+let bookObject = JSON.parse(bookString) as book;
+
+let value:any;
+
+value ="arkit";
+value = [1,2,3];
+value=2.5;
+  value.toUpperCase();
+
+let newvalue:unknown;
+
+newvalue ="arkit";
+newvalue = [1,2,3];
+newvalue=2.5;
+if(typeof newvalue==="string")
+newvalue.toUpperCase();
+////
+
+try{}catch(error){
+    if(error instanceof Error){
+        console.log(error)
+    }
+    console.log("Error",error);
+    
+}
+
+
+
+
+
+
+
