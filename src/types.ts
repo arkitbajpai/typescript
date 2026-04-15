9a424bf7e2ccdb5becc6619480e27b89c00ddc6f
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-04-15 14:26:49 +0530

    types of the tyscript

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+let drink ="chai"
+// infer the data to string we have not decalare it.
+ let cups =Math.random()>0.5 ? 10:5 
+
+//  //the compiler's ability to automatically determine and assign types to variables, function returns, and expressions based on their assigned values and context 
+// TypeScript is known as type infrencing
+
+let name :string= "Arkit"  // type anottation we can overide it also.
+
