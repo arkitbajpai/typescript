16614b472c4d85c57d2649d1ed25c7d3737eb222
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-04-22 16:22:15 +0530

    oops more

--- src/opss.ts
+++ src/opss.ts
@@ -40,3 +40,54 @@ class Walet{
     #balance=100// this is the private decaltion.
 
 }
+
+class Cup{
+    readonly capacity:number=250
+    constructor(capacity:number){
+        this.capacity= capacity
+    }
+}
+
+
+class MorderChai{
+    private _sugar=2
+
+    get sugar(){
+        return this._sugar
+    }
+
+    set sugar(value:number){
+        if(value>5)
+            throw new Error("too sweets")
+        this._sugar= value
+    }
+}
+
+const ct = new MorderChai()
+ct.sugar=3
+
+class Ekchai{
+    static shopName ="Chai Code Caffe"
+
+    constructor(public flavour: string){
+
+    }
+
+}
+
+
+class Heater{
+    heat(){
+
+    }
+}
+
+class ChaiMaker{
+    constructor(private heater: Heater){
+
+    }
+    make(){
+        this.heater.heat
+    }
+}
+
